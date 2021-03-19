db8889a71630f5003d8106e1bca9726d57528922
abdullahmohsen98 <abdo2dd@gmail.com>
2021-03-19 18:44:08 +0200

    Updating Project (partitioning it into folders)

---
+++ Library-Mangement-system/Library-Mangement-system/database.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+
+namespace Library_Mangement_system
+{
+    class database
+    {
+        public static SqlConnection db()
+        {
+            SqlConnection con = new SqlConnection(@"Server=DESKTOP-GM27C9I; database=Library; integrated security=SSPI");
+            return con;
+        }
+
+    }
+}
