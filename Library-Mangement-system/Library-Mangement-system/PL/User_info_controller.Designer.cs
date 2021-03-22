d4f42ab399cdc808b683eb87c3bb4c2f4873cf8d
abdullahmohsen98 <abdo2dd@gmail.com>
2021-03-23 00:49:13 +0200

    Designing form of User Info

---
+++ Library-Mangement-system/Library-Mangement-system/PL/User_info_controller.Designer.cs
@@ -0,0 +1,208 @@
+﻿
+namespace Library_Mangement_system.PL
+{
+    partial class User_info_controller
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(User_info_controller));
+            this.create_acc = new System.Windows.Forms.Button();
+            this.label5 = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            this.user_ssn = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.user_phone = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.user_address = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.user_name = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // create_acc
+            // 
+            this.create_acc.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.create_acc.Location = new System.Drawing.Point(220, 443);
+            this.create_acc.Name = "create_acc";
+            this.create_acc.Size = new System.Drawing.Size(197, 41);
+            this.create_acc.TabIndex = 23;
+            this.create_acc.Text = "Create New Account";
+            this.create_acc.UseVisualStyleBackColor = true;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Monotype Corsiva", 20.25F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(206, 10);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(236, 33);
+            this.label5.TabIndex = 22;
+            this.label5.Text = "Account Information";
+            // 
+            // button2
+            // 
+            this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button2.Location = new System.Drawing.Point(385, 373);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(197, 41);
+            this.button2.TabIndex = 21;
+            this.button2.Text = "Change Information ";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // user_ssn
+            // 
+            this.user_ssn.Enabled = false;
+            this.user_ssn.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.user_ssn.Location = new System.Drawing.Point(220, 273);
+            this.user_ssn.Name = "user_ssn";
+            this.user_ssn.ReadOnly = true;
+            this.user_ssn.Size = new System.Drawing.Size(222, 24);
+            this.user_ssn.TabIndex = 20;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(98, 273);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(91, 20);
+            this.label4.TabIndex = 19;
+            this.label4.Text = "National id :";
+            // 
+            // user_phone
+            // 
+            this.user_phone.Enabled = false;
+            this.user_phone.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.user_phone.Location = new System.Drawing.Point(220, 208);
+            this.user_phone.Name = "user_phone";
+            this.user_phone.ReadOnly = true;
+            this.user_phone.Size = new System.Drawing.Size(222, 24);
+            this.user_phone.TabIndex = 18;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(98, 208);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(63, 20);
+            this.label3.TabIndex = 17;
+            this.label3.Text = "Phone :";
+            // 
+            // user_address
+            // 
+            this.user_address.Enabled = false;
+            this.user_address.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.user_address.Location = new System.Drawing.Point(220, 142);
+            this.user_address.Name = "user_address";
+            this.user_address.ReadOnly = true;
+            this.user_address.Size = new System.Drawing.Size(222, 24);
+            this.user_address.TabIndex = 16;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(98, 142);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(76, 20);
+            this.label2.TabIndex = 15;
+            this.label2.Text = "Address :";
+            // 
+            // user_name
+            // 
+            this.user_name.Enabled = false;
+            this.user_name.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.user_name.Location = new System.Drawing.Point(220, 78);
+            this.user_name.Name = "user_name";
+            this.user_name.ReadOnly = true;
+            this.user_name.Size = new System.Drawing.Size(222, 24);
+            this.user_name.TabIndex = 14;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(98, 78);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(97, 20);
+            this.label1.TabIndex = 13;
+            this.label1.Text = "User Name :";
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(66, 373);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(197, 41);
+            this.button1.TabIndex = 12;
+            this.button1.Text = "Change Password ";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // User_info_controller
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(649, 495);
+            this.Controls.Add(this.create_acc);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.user_ssn);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.user_phone);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.user_address);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.user_name);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "User_info_controller";
+            this.Text = "User_info_controller";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button create_acc;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.TextBox user_ssn;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox user_phone;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox user_address;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox user_name;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button1;
+    }
+}
